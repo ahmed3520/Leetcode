dd83277533a900a6f997c84e4559f3505483410d
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-05-10 19:40:41 +0200

    Time: 4 ms (73.16%), Space: 3.5 MB (10.39%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.go
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.go
@@ -1,12 +1,16 @@
 func finalValueAfterOperations(operations []string) int {
-    x:=0
-    //improved solution 
-    for i:=0; i< len(operations); i++ {
-        if (operations[i] == "X++" || operations[i] == "++X"){
+    x := 0
+    
+    for i := 0; i < len(operations); i++ {
+        op := operations[i]
+        if op == "X++" || op == "++X" {
             x++
-        }else {
+        } else if op == "X--" || op == "--X" {
             x--
+        } else {
+            //check if there any hidden test cased
         }
-}
-        return x
+    }
+    
+    return x
 }

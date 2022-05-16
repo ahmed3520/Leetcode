86cdb29e8ed231511149ea77cff4f932d1acd912
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-05-16 18:45:12 +0200

    Time: 133 ms (28.01%), Space: 9.5 MB (16.30%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.go
+++ 11-container-with-most-water/11-container-with-most-water.go
@@ -1,24 +1,23 @@
-func min(x, y int)int{
-    if x <= y {
-        return x
-    }
-    
-    return y
+func maxArea(height []int) int {
+    	max := 0
+	i, j := 0, len(height)-1
+	for i < j {
+		area := (j - i) * min(height[i], height[j])
+		if area > max {
+			max = area
+		}
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return max
 }
 
-func maxArea(height []int) int {
-    maximum:=0
-    l,r := 0, len(height)-1
-    for l <r {
-        area := (r-l) * min(height[l],height[r])
-        if area > maximum {
-            maximum = area
-        }
-        if height[l]<height[r]{
-            l++
-        }else {
-            r--
-        }
-    }
-    return maximum
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }

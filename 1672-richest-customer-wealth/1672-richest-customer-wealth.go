6a1e751f751dd92432bbab56ce4e5c02b2a826c5
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-06-09 03:13:33 +0200

    Time: 3 ms (87.06%), Space: 3.1 MB (62.44%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.go
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.go
@@ -1,17 +1,21 @@
-func maximumWealth(accounts [][]int) int {
-    max:=0
-    for i := range accounts {
-        temp:=0
-        for j := range accounts[i]{
-         temp+= accounts[i][j]   
-        }
-        max = getMaximum(max,temp)
-    }
-    return max
-}
-func getMaximum(a,b int)int{
-    if a >b{
+func max(a, b int) int {
+    if a > b {
         return a
     }
+    
     return b
+}
+
+func maximumWealth(accounts [][]int) int {
+    wealth := 0
+    
+    for i := range accounts {
+        sum := 0
+        for j := range accounts[i] {
+            sum += accounts[i][j]
+        }
+        wealth = max(wealth, sum)
+    }
+    
+    return wealth
 }

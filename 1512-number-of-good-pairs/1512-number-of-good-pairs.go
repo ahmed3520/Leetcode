4b5200aa53d77e6b0e24456ed6cbfac47131d0fa
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-06-09 19:16:51 +0200

    Time: 0 ms (100.00%), Space: 2 MB (27.27%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.go
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.go
@@ -2,7 +2,6 @@ func numIdenticalPairs(nums []int) int {
    dict := [128]int{}
     count := 0
     for _,val := range nums {
-        
             count += dict[val]
             dict[val]++
         

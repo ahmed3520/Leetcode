04367fadba7f2830313f90a39fb8f856c0c59fb7
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-06-12 18:27:18 +0200

    Time: 369 ms (28.00%), Space: 9.6 MB (32.00%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.go
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.go
@@ -1,27 +1,28 @@
+
 func maximumUniqueSubarray(nums []int) int {
-    cur_sum, ans, l  :=   0,0,0
-    s := map[int]bool{}
-    for r,_ := range nums{
-    for{   
-        if _,ok := s[nums[r]]; ok {
-        delete(s, nums[l])
-			cur_sum -= nums[l]
-			l += 1
-        }else{
-            break
-            
-        }
-        }
-        s[nums[r]] = true
-		         
-		cur_sum += nums[r]         
+	cur_sum, ans, l := 0, 0, 0
+	s := map[int]bool{}
+	for r, _ := range nums {
+		for {
+			if _, ok := s[nums[r]]; ok {
+				delete(s, nums[l])
+				cur_sum -= nums[l]
+				l += 1
+			} else {
+				break
+
+			}
+		}
+		s[nums[r]] = true
+
+		cur_sum += nums[r]
 		ans = max(ans, cur_sum)
-    }                               
+	}
 	return ans
 }
-func max(a,b int) int{
-    if a > b{
-        return a
-    }
-    return b
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }

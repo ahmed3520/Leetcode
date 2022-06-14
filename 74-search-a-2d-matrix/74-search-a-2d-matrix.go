140c7bdae97ff75bcf5f4cffbdab82b62115b9f2
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-06-14 04:11:19 +0200

    Time: 0 ms (100.00%), Space: 2.7 MB (67.41%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.go
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.go
@@ -1,21 +1,37 @@
 func searchMatrix(matrix [][]int, target int) bool {
-	left, right := 0, len(matrix)*len(matrix[0])
-	for left < right {
-		middle := (left + right) / 2
-		i, j := middle/len(matrix[0]), middle%len(matrix[0])
-
-		if target < matrix[i][j] {
-			right = middle
-			continue
-		}
-
-		if target > matrix[i][j] {
-			left = middle + 1
-			continue
-		}
-
-		return true
-	}
-
-	return false
+ 
+    row,col:=len(matrix),len(matrix[0])
+    top:=0
+    bot:=row-1
+    
+    for top<=bot{
+        row=(top+bot)/2
+        if target > matrix[row][col-1]{
+            top=row+1
+        }else if target<matrix[row][0]{
+            bot=row-1
+        }else{
+            break
+        }
+    }
+    
+    if ! (top <=bot){
+        return  false
+    }
+    
+    row=(top+bot)/2
+    l,r:=0,col-1
+    for l<=r{
+        m:=(l+r)/2
+        if target> matrix[row][m]{
+            l=m+1
+        } else if target< matrix[row][m]{
+            r=m-1
+        } else{
+            return true
+        }
+        
+    }
+    return false
+    
 }

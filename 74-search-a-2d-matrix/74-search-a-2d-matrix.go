6796d96e0fafaf3d7f2754489bffd1018b8e56de
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-06-14 04:09:53 +0200

    Time: 9 ms (6.68%), Space: 2.7 MB (67.41%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.go
@@ -0,0 +1,21 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	left, right := 0, len(matrix)*len(matrix[0])
+	for left < right {
+		middle := (left + right) / 2
+		i, j := middle/len(matrix[0]), middle%len(matrix[0])
+
+		if target < matrix[i][j] {
+			right = middle
+			continue
+		}
+
+		if target > matrix[i][j] {
+			left = middle + 1
+			continue
+		}
+
+		return true
+	}
+
+	return false
+}

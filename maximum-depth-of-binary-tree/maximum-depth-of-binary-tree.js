1271654178109f143c0c8294c2bfcb216604a008
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2022-08-01 16:01:11 +0200

    Time: 292 ms, Memory: 49.8 MB - LeetHub

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let res=0
+    if(root==null){
+        return 0
+    }
+   let leftRes = maxDepth(root.left)
+   let rightRes = maxDepth(root.right)
+   console.log(leftRes,rightRes)
+    res=Math.max(leftRes,rightRes)+1
+    return res
+};
